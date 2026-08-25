9fde7d6d0e74633997c4ae1d88335ec0c6f87f01
scrollingcat <119154909+scrollingcat@users.noreply.github.com>
2026-08-25 22:01:50 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    void helper(vector<int>nums, vector<vector<int>>&res, int ind)
+    {
+        if (ind == nums.size())
+        {
+            res.push_back(nums);
+            return;
+        }
+        for (int i = ind; i < nums.size(); i++)
+        {
+            swap(nums[ind], nums[i]); 
+            helper(nums, res, ind + 1);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        helper(nums, res, 0);
+        return res;
+    }
+};
